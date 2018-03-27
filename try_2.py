3f15baa4ec62374225bc7f964b4109a4f62cf82d
Mark Ma <ken.ma@integratto.co.jp>
2018-03-27 17:45:50 +0900

    add page counter

--- try_2.py
+++ try_2.py
@@ -5,7 +5,7 @@ from selenium.webdriver.chrome.options import Options
 
 url ='http://localhost:7000/#!/panel'
 
-# keyword
+# Open Chrome
 chrome_options = Options()
 chrome_options.add_argument('headless')
 driver = webdriver.Chrome(chrome_options=chrome_options)
@@ -13,13 +13,14 @@ driver.get(url)
 num = driver.find_element_by_id("app").text
 print(num)
 
+#Window handles
 now_handle = driver.current_window_handle
 #Click the button, Open a New window for English version
 driver.find_element_by_class_name("btn").click()
 #Get the handle information from all windows
 all_handles = driver.window_handles
 for handle in all_handles:
-    #check the new window handle
+    #Check the new window's handle
     if handle != now_handle:
         print(handle)
       #Use the new window, band with it
